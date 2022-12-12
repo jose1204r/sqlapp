46295de63ba82825b443696eeab027fbf74bb30d
jose <jrodriguez@itsolutionspcllc.com>
2022-12-11 21:09:22 -0500

    SQLapp

---
+++ SQLapp/IDepartmentRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLapp
+{
+    internal interface IDepartmentRepository
+    {
+        public interface IDepartmentRepository
+        {
+            IEnumerable<Department> GetAllDepartments(); //Stubbed out method
+        }
+
+
+
+    }
+}
---
+++ SQLapp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace SQLapp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            var config = new ConfigurationBuilder()
+                 .SetBasePath(Directory.GetCurrentDirectory())
+                 .AddJsonFile("appsettings.json")
+                 .Build();
+
+            string connString = config.GetConnectionString("DefaultConnection");
+            IDbConnection conn = new MySqlConnection(connString);
+
+
+            var  repo = new DapperDepartmentRepository(conn);
+            var departments = repo.GetAllDepartments();
+            Console.WriteLine("*************Best Buy Departments**************");
+            foreach (var item in departments)
+            {
+                Console.WriteLine($"{item.DepartmentID}    {item.Name}");
+            }
+
+        }
+    }
+}
